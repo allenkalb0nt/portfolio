e5e9c5f6907f954e354d92d14a0ae5c1f3285834
AllenKalboh <tmc.patrickallen.casili@cvsu.edu.ph>
2026-02-09 03:04:03 +0800

    Update Contact.tsx

--- src/app/_components/Contact.tsx
+++ src/app/_components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
             </div>
 
             <div className='border border-gray-300 dark:border-gray-700 p-3 md:p-4 rounded-md hover:scale-105 hover:shadow-lg duration-300 cursor-pointer flex items-center justify-center'>
-                <p className="text-xs sm:text-sm md:text-base flex items-center"> <Instagram className="inline-block mr-2 w-4 h-4 sm:w-5 sm:h-5" /><Link href="https://www.instagram.com/triksyuy/" target="_blank" rel="noopener noreferrer" className="text-blue-600 dark:text-blue-400 hover:underline">Patrick Allen</Link></p>
+                <p className="text-xs sm:text-sm md:text-base flex items-center"> <Instagram className="inline-block mr-2 w-4 h-4 sm:w-5 sm:h-5" /><Link href="https://www.instagram.com/triksyuy/" target="_blank" rel="noopener noreferrer" className=" hover:underline">Patrick Allen</Link></p>
             </div>
         </div>
     )
