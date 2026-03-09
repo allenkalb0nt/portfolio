12fde133ac85e6546e9851fc0ef2e3d40d2d3c9f
AllenKalboh <patrickallencasili@gmail.com>
2026-03-09 16:22:06 +0800

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Poppins, Geist_Mono } from "next/font/google";
+import { Poppins, Geist_Mono, Geist } from "next/font/google";
 import "./globals.css";
 import NavBar from "./_components/NavBar";
 import { ThemeProvider } from "next-themes";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -26,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body
         className={`${poppins.variable} ${geistMono.variable} antialiased`}
       >
